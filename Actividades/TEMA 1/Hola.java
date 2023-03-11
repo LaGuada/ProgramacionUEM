3b703d1d7b87c7a37a368fb2e751358f71a609ea
maven <guadalupevarlo@gmail.com>
2023-03-11 20:00:53 +0100

    cambio github.com/

--- Actividades/TEMA 1/Hola.java
+++ Actividades/TEMA 1/Hola.java
@@ -1,9 +1,9 @@
 
 public class Hola {
 
-    public static void  main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("hola java");
-        System.out.println("Este es nuestro primer programa editado en VSCode");
+        System.out.println("Este es nuestro primer programa editado en VSCode y subido a GitHub");
     }
-    
+
 }
